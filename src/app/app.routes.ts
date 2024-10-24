04bdf36b25fe44c2f461a1dc935bbe1392ef858d
Nicolás Jurado <99767358+NicholashiG@users.noreply.github.com>
2024-10-23 21:16:17 -0500

    Se hace la guía 15
    Se hace la guía 15, toca revisar lo de la contraseña y lo de crearEvento

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,15 @@ import { InicioComponent } from './componentes/inicio/inicio.component';
 import { LoginComponent } from './componentes/login/login.component';
 import { RegistroComponent } from './componentes/registro/registro.component';
 import { CrearEventoComponent } from './componentes/crear-evento/crear-evento.component';
-
+import { HeaderComponent } from './componentes/header/header.component';
+import { FooterComponent } from './componentes/footer/footer.component';
 
 export const routes: Routes = [
    { path: '', component: InicioComponent },
    { path: 'login', component: LoginComponent },
    { path: 'registro', component: RegistroComponent },
    { path: 'eventos/crear-evento', component: CrearEventoComponent},
+   { path: 'header', component: HeaderComponent},
+   { path: 'footer', component: FooterComponent},
    { path: "**", pathMatch: "full", redirectTo: "" }
 ];
